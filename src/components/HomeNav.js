c3131f71b4e5cee59d923dfe6d5a94a5da5f4c68
Phillip Lorenzo <philliplorenzo@Phillips-MacBook-Pro.local>
2016-11-18 09:47:09 -0800

    Refactored Quote.js to take in props, refactored all css files, including fixing media breakpoints, and cleaned up extra whitespace

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import NavMenu from './NavMenu'
 import '../styles/home_nav.css'
 
 class HomeNav extends Component {
@@ -34,30 +35,6 @@ class HomeNav extends Component {
   }
 }
 
-let NavMenu = React.createClass({
-  getDefaultProps() {
-    return {
-      isOpen: false
-    }
-  },
-
-  render() {
-    if(this.props.isOpen)
-    {
-    return <div className="home-nav-dropdown-container">
-      <div className="home-nav-triangle"></div>
-      <div className="home-nav-dropdown">
-        <ul className="home-nav-dropdown-content">
-          <li><a href="#">INTEGRATIONS</a></li>
-          <li><a href="#">DASHBOARD EXAMPLES</a></li>
-          <li><a href="#">KPI EXAMPLES</a></li>
-        </ul>
-      </div>
-    </div>
-    }
-    return null
-  }
-})
 
 let NavMenuButton = React.createClass({
   getInitialState() {
---
+++ src/components/NavMenu.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+import '../styles/home_nav.css'
+
+class NavMenu extends Component {
+  getDefaultProps() {
+    return {
+      isOpen: false
+    }
+  }
+
+  render() {
+    if(this.props.isOpen) {
+    return <div className="home-nav-dropdown-container">
+      <div className="home-nav-triangle"></div>
+      <div className="home-nav-dropdown">
+        <ul className="home-nav-dropdown-content">
+          <li><a href="#">INTEGRATIONS</a></li>
+          <li><a href="#">DASHBOARD EXAMPLES</a></li>
+          <li><a href="#">KPI EXAMPLES</a></li>
+        </ul>
+      </div>
+    </div>
+  }
+    return null
+  }
+}
+
+export default NavMenu
