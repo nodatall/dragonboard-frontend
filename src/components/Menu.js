83e788ba5b896d2d80b791895782fcd356304b54
anasauce <anasauceda1843@gmail.com>
2016-12-14 15:46:49 -0800

    minor changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,14 +4,12 @@ import '../styles/menu.css'
 class Menu extends Component {
   render() {
     if (this.props.active) {
-      return (
-        <div className="account-menu">
+      return <div className="account-menu">
           <div className="account-menu-list">
             <div className="account-menu-list-item"><a href="#">Account</a></div>
             <div className="account-menu-list-item"><a href="#">Logout</a></div>
           </div>
         </div>
-      )
     } else {
       return null
     }
