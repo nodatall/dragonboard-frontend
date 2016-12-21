cca12d722efabf1f6f16bbaf2f296d2b3896d4cc
jason00111 <emberleyjason@gmail.com>
2016-12-21 09:27:07 -0800

    Put HomeNavMenuDropdownButton in its own file [Resolves #74]  - Refactored into ES6 class

---
+++ src/components/landing/navigation/DropdownButton.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+import HomeNavDropdownMenu from './Menu'
+
+import '../../../styles/landing/navigation/dropdown_button.css'
+
+export default class HomeNavMenuDropdownButton extends Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggleMenu( event ) {
+    this.setState( { isOpen: !this.state.isOpen } )
+  }
+
+  onClose() {
+    this.setState( { isOpen: false } )
+  }
+
+  componentDidMount() {
+    document.body.addEventListener( 'click', this.onClose.bind( this ) )
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener( 'click', this.onClose.bind( this ) )
+  }
+
+  render() {
+    return (
+      <div>
+        <a onClick={ this.toggleMenu.bind( this ) } href='#'>More
+          <div className='more-triangle' />
+        </a>
+        <HomeNavDropdownMenu isOpen={ this.state.isOpen } />
+      </div>
+    )
+  }
+}
